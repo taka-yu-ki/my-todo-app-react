4974aeaff8bf026151483a751597dad6a7a207a3
taka-yu-ki <taka7_zegs@icloud.com>
2023-07-02 10:58:09 +0900

    complete 20230702

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Todo from "./Todo";
 
-const TodoList = () => {
-  return <div>TodoList</div>;
+const TodoList = ({ todos, toggleTodo }) => {
+  return todos.map((todo) => (
+    <Todo todo={todo} key={todo.id} toggleTodo={toggleTodo} />
+  ));
 };
 
 export default TodoList;
